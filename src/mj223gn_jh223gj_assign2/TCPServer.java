12719fa050ca8ec8881dfd5260b257ac1588faf3
kaikun213 <kaktus.w.h@gmx.de>
2017-02-08 10:53:06 +0100

    gitIgnore updated

--- src/mj223gn_jh223gj_assign2/TCPServer.java
+++ src/mj223gn_jh223gj_assign2/TCPServer.java
@@ -2,7 +2,6 @@ package mj223gn_jh223gj_assign2;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
