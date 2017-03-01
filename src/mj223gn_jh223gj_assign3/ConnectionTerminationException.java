5026b4ec98efcfaf811aef87c8126f27e9836ce5
kaikun213 <kaktus.w.h@gmx.de>
2017-03-01 17:41:48 +0100

    ConnectionTerminationException

---
+++ src/mj223gn_jh223gj_assign3/ConnectionTerminationException.java
@@ -0,0 +1,14 @@
+package mj223gn_jh223gj_assign3;
+
+public class ConnectionTerminationException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ConnectionTerminationException(){
+		super();
+	}
+
+}
